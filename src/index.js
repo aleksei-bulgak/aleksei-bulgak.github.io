58338d6e4e94f05d684876c1823059b23b79f3aa
aleksei-bulgak <aleksei.bulgak@gmail.com>
2019-06-25 15:17:14 +0300

    styles: tune photo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import '../styles/styles.scss';
 
 appendStyleWhenInViewPort('.trainings__item', 'trainings__item--animated');
+easterEgg();
 
 function appendStyleWhenInViewPort(elementSelector, styleClass) {
   window.addEventListener('scroll', () => {
-    document.body
-      .querySelectorAll(elementSelector)
-      .forEach(e => {
-        if(isInViewPort(e) && !e.classList.contains(styleClass)) {
-          e.classList.add(styleClass);
-        }
-      });
+    document.body.querySelectorAll(elementSelector).forEach(e => {
+      if (isInViewPort(e) && !e.classList.contains(styleClass)) {
+        e.classList.add(styleClass);
+      }
+    });
   });
 }
 
 function isInViewPort(element) {
   const bounding = element.getBoundingClientRect();
-  if (
-    (bounding.bottom + bounding.top) / 2.5 <
-    (window.innerHeight || document.documentElement.clientHeight)
-  ) {
+  if ((bounding.bottom + bounding.top) / 2.5 < (window.innerHeight || document.documentElement.clientHeight)) {
     return true;
   }
 
   return false;
+}
+
+function easterEgg() {
+  let counter = 0;
+  document.body.querySelector('.intro__photo>img').addEventListener('click', e => {
+    counter += 1;
+    if (counter === 10) {
+      e.target.parentElement.classList.add('egg');
+    } else {
+      e.target.parentElement.classList.remove('egg');
+    }
+  });
 }
