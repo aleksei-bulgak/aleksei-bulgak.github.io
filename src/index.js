7989741a8e03ab0620c06f76907de646138f0c54
aleksei-bulgak <aleksei.bulgak@gmail.com>
2019-06-25 11:51:37 +0300

    feat: implement footer, education.
    add some animantion for courses icons
    change some styles and values

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import '../styles/styles.scss';
+import "../styles/styles.scss";
+
+appendStyleWhenInViewPort(".trainings__item", "trainings__item--animated");
+
+function appendStyleWhenInViewPort(elementSelector, styleClass) {
+  window.addEventListener("scroll", () => {
+    document.body
+      .querySelectorAll(elementSelector)
+      .forEach(e => {
+        if(isInViewPort(e) && !e.classList.contains(styleClass)) {
+          e.classList.add(styleClass);
+        }
+      });
+  });
+}
+
+function isInViewPort(element) {
+  const bounding = element.getBoundingClientRect();
+  if (
+    (bounding.bottom + bounding.top) / 2.5 <
+    (window.innerHeight || document.documentElement.clientHeight)
+  ) {
+    return true;
+  }
+
+  return false;
+}
