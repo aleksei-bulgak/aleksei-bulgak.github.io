050dd78b2705c5444ca8533e9bcdb3f1f1e6b24c
aleksei-bulgak <aleksei.bulgak@gmail.com>
2019-06-26 21:17:13 +0300

    styles: add post css configuration with autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.[hash].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.[hash].js'
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})]
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
@@ -28,21 +36,23 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin(),
-    new ExtractTextPlugin("style.css"),
+    new MiniCssExtractPlugin({
+      filename: 'style.[hash].css',
+      chunkFilename: '[id].[hash].css'
+    }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
-      hash: true
+      template: './index.html'
     }),
     new CopyWebpackPlugin([
       {
-        from: "./assets",
-        to: "../dist/assets"
+        from: './assets',
+        to: '../dist/assets'
       }
     ])
   ],
 
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
   }
