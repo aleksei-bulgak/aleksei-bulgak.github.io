75054bbb2d84f0977bf95e7261063a3574100e1e
aliaksei.bulhak <aliaksei_bulhak@epam.com>
2019-12-09 11:03:32 +0300

    fix: error with hidden courses on IE11

--- src/index.js
+++ src/index.js
@@ -10,9 +10,23 @@ const intersectionObserver = new IntersectionObserver(elements => {
     .forEach(e => {
       e.target.classList.add('trainings__item--animated');
       intersectionObserver.unobserve(e.target);
+      intersectionObserverHide.unobserve(e.target);
     });
 }, options);
 
+const intersectionObserverHide = new IntersectionObserver(elements => {
+  elements
+    .filter(e => e.isIntersecting)
+    .forEach(e => {
+      e.target.classList.remove('trainings__item--animated');
+      intersectionObserverHide.unobserve(e.target);
+    });
+}, {
+  rootMargin: '200px',
+  threshold: 0
+});
+
 [...document.querySelectorAll('.trainings__item')].forEach(elem => {
+  intersectionObserverHide.observe(elem);
   intersectionObserver.observe(elem);
 });
