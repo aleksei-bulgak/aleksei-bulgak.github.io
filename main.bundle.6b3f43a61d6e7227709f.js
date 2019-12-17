d1137d8ed2d9b5baeb5561333fb3b07132893642
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 07:36:19 +0000

    Deploy aleksei-bulgak/aleksei-bulgak.github.io to github.com/aleksei-bulgak/aleksei-bulgak.github.io.git:master

--- main.bundle.6b3f43a61d6e7227709f.js
+++ main.bundle.6b3f43a61d6e7227709f.js
@@ -98,16 +98,34 @@ __webpack_require__.r(__webpack_exports__);
 const options = {
   threshold: 0.5
 };
+
+const intersectionObserverHide = new IntersectionObserver(
+  elements => {
+    elements
+      .filter(e => e.isIntersecting)
+      .forEach(e => {
+        e.target.classList.remove('trainings__item--animated');
+        intersectionObserverHide.unobserve(e.target);
+      });
+  },
+  {
+    rootMargin: '200px',
+    threshold: 0
+  }
+);
+
 const intersectionObserver = new IntersectionObserver(elements => {
   elements
     .filter(e => e.isIntersecting)
     .forEach(e => {
       e.target.classList.add('trainings__item--animated');
       intersectionObserver.unobserve(e.target);
+      intersectionObserverHide.unobserve(e.target);
     });
 }, options);
 
 [...document.querySelectorAll('.trainings__item')].forEach(elem => {
+  intersectionObserverHide.observe(elem);
   intersectionObserver.observe(elem);
 });
 
